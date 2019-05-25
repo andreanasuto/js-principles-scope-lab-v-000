51ab24de22d4ed8e95e7317a94d5096356185754
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-25 00:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write your solution in this file!
-const customerName = 'bob'
+let customerName = 'bob'
 
 function upperCaseCustomerName() {
   customerName = 'BOB'
+}
+
+function setBestCustomer() {
+  const bestCustomer
+
+  let b
 }
